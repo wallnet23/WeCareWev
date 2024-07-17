264b472077fa9970c636c4cf9d7b4b3ecb68a151
wallnetMac <wallnetmac1@gmail.com>
2024-07-17 13:02:40 +0200

    Rivista totalmente visualizzazione pagina readonly ed aggiunta completa step-four. Aggiornamento ed aggiunta elementi footer. Aggiornamento colore navbar

--- src/app/pages/systems/interfaces/system-info-full.ts
+++ src/app/pages/systems/interfaces/system-info-full.ts
@@ -2,11 +2,15 @@ import { StepFour } from "../components/interfaces/step-four";
 import { StepOne } from "../components/interfaces/step-one";
 import { StepThree } from "../components/interfaces/step-three";
 import { StepTwo } from "../components/interfaces/step-two";
+import { ClusterData } from "./clusterData";
+import { Inverter, InverterData } from "./inverterData";
 
 export interface SystemInfoFull {
     id: number;
     stepOne: StepOne;
     stepTwo: StepTwo;
     stepThree: StepThree;
-    stepFour?: StepFour;
+    stepFour: StepFour;
+    cluster: ClusterData;
+    inverter: InverterData;
 }
